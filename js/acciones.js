ce6c35606c14f57b74f79d6690932e5a87d98f7b
AlexisMedina <damcb164e22@gmail.com>
2015-11-18 11:41:53 -0600

    2subida
    app

--- js/acciones.js
+++ js/acciones.js
@@ -2,6 +2,10 @@
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
 	
+	$("#btnconsulta").on("tap", function(){
+		alert("Preguntando...");
+		consulta();
+	});
 }); 
 });
 
