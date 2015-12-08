79ce085440f9c0909f051a6413f34ecfbb163741
AlexisMedina <damcb164e22@gmail.com>
2015-12-08 09:13:16 -0600

    42
    ipicas

--- js/acciones.js
+++ js/acciones.js
@@ -7,7 +7,7 @@ function consulta()
 	datos;
     $.ajax({  
 		type: "POST",
-		url: "http://192.168.1.171/libreria/buscarTodos.php",
+		url: "http://192.168.1.191/libreria/buscarTodos.php",
 		data: datos, 
 		error: function() {  
 			alert("Error, no se puede conectar al servidor");
@@ -19,7 +19,7 @@ function consulta()
 			transition: 'pop' 
 		 });  
 		 for (var x=0; x<datosLibros.Libros.length; x++) { 6
-			 $('#contenido').append("<div><div align='center'><div style='display:inline-block' align='center'><img src='http://192.168.1.171/proyecto/recursos/imagenes/libros/" + datosLibros.Libros[x].claveLibro + ".jpg' width='60%' height='auto' /></div><div style='display:inline-block' align='center'><h1>" + datosLibros.Libros[x].tituloLibro + "</h1><br><h2>" + datosLibros.Libros[x].autorLibro + "</h2><div style='display:inline-block' align='center'><h3>Editorial: " + datosLibros.Libros[x].editorialLibro + "</h3></div><div style='display:inline-block' align='center'><h3>Número de paginas: " + datosLibros.Libros[x].paginasLibro + "</h3></div></div></div><div align='center'><h2>Precio: $" + datosLibros.Libros[x].precioventa + "</h2></div>  <div>  <h2>Sinopsis: </h2> <br> <center> <p>" + datosLibros.Libros[x].sinopsisLibro + "</p> </center>  <br><center>" + datosLibros.Libros[x].generoLibro + "</center></div></div>");
+			 $('#contenido').append("<div><div align='center'><div style='display:inline-block' align='center'><img src='http://192.168.1.191/proyecto/recursos/imagenes/libros/" + datosLibros.Libros[x].claveLibro + ".jpg' width='60%' height='auto' /></div><div style='display:inline-block' align='center'><h1>" + datosLibros.Libros[x].tituloLibro + "</h1><br><h2>" + datosLibros.Libros[x].autorLibro + "</h2><div style='display:inline-block' align='center'><h3>Editorial: " + datosLibros.Libros[x].editorialLibro + "</h3></div><div style='display:inline-block' align='center'><h3>Número de paginas: " + datosLibros.Libros[x].paginasLibro + "</h3></div></div></div><div align='center'><h2>Precio: $" + datosLibros.Libros[x].precioventa + "</h2></div>  <div>  <h2>Sinopsis: </h2> <br> <center> <p>" + datosLibros.Libros[x].sinopsisLibro + "</p> </center>  <br><center>" + datosLibros.Libros[x].generoLibro + "</center></div></div>");
 		 } 
 		 
 		} 
@@ -31,7 +31,7 @@ function consultatitulo()
 	datos="titulo=" + $('#txttitulo').val();
     $.ajax({  
 		type: "POST",
-		url: "http://192.168.1.171/libreria/buscarTitulo.php",
+		url: "http://192.168.1.191/libreria/buscarTitulo.php",
 		data: datos, 
 		error: function() {  
 			alert("Error, no se puede conectar al servidor");
@@ -44,7 +44,7 @@ function consultatitulo()
 			transition: 'pop' 
 		 });  
 		 for (var x=0; x<datosLibros.Libros.length; x++) { 6
-			 $('#contenido2').append("<div><div align='center'><div style='display:inline-block' align='center'><img src='http://192.168.1.171/proyecto/recursos/imagenes/libros/" + datosLibros.Libros[x].claveLibro + ".jpg' width='60%' height='auto' /></div><div style='display:inline-block' align='center'><h1>" + datosLibros.Libros[x].tituloLibro + "</h1><br><h2>" + datosLibros.Libros[x].autorLibro + "</h2><div style='display:inline-block' align='center'><h3>Editorial: " + datosLibros.Libros[x].editorialLibro + "</h3></div><div style='display:inline-block' align='center'><h3>Número de paginas: " + datosLibros.Libros[x].paginasLibro + "</h3></div></div></div><div align='center'><h2>Precio: $" + datosLibros.Libros[x].precioventa + "</h2></div>  <div>  <h2>Sinopsis: </h2> <br> <center> <p>" + datosLibros.Libros[x].sinopsisLibro + "</p> </center>  <br><center>" + datosLibros.Libros[x].generoLibro + "</center></div></div>");
+			 $('#contenido2').append("<div><div align='center'><div style='display:inline-block' align='center'><img src='http://192.168.1.191/proyecto/recursos/imagenes/libros/" + datosLibros.Libros[x].claveLibro + ".jpg' width='60%' height='auto' /></div><div style='display:inline-block' align='center'><h1>" + datosLibros.Libros[x].tituloLibro + "</h1><br><h2>" + datosLibros.Libros[x].autorLibro + "</h2><div style='display:inline-block' align='center'><h3>Editorial: " + datosLibros.Libros[x].editorialLibro + "</h3></div><div style='display:inline-block' align='center'><h3>Número de paginas: " + datosLibros.Libros[x].paginasLibro + "</h3></div></div></div><div align='center'><h2>Precio: $" + datosLibros.Libros[x].precioventa + "</h2></div>  <div>  <h2>Sinopsis: </h2> <br> <center> <p>" + datosLibros.Libros[x].sinopsisLibro + "</p> </center>  <br><center>" + datosLibros.Libros[x].generoLibro + "</center></div></div>");
 		 } 
 		 
 		} 
@@ -57,7 +57,7 @@ function consultaautor()
 	var datos="autor=" + $('#txtautor').val();
     $.ajax({  
 		type: "POST",
-		url: "http://192.168.1.171/libreria/buscarAutor.php",
+		url: "http://192.168.1.191/libreria/buscarAutor.php",
 		data: datos, 
 		error: function() {  
 			alert("Error, no se puede conectar al servidor");
@@ -70,7 +70,7 @@ function consultaautor()
 			transition: 'pop' 
 		 });  
 		 for (var x=0; x<datosLibros.Libros.length; x++) { 6
-			 $('#contenido3').append("<div><div align='center'><div style='display:inline-block' align='center'><img src='http://192.168.1.171/proyecto/recursos/imagenes/libros/" + datosLibros.Libros[x].claveLibro + ".jpg' width='60%' height='auto' /></div><div style='display:inline-block' align='center'><h1>" + datosLibros.Libros[x].tituloLibro + "</h1><br><h2>" + datosLibros.Libros[x].autorLibro + "</h2><div style='display:inline-block' align='center'><h3>Editorial: " + datosLibros.Libros[x].editorialLibro + "</h3></div><div style='display:inline-block' align='center'><h3>Número de paginas: " + datosLibros.Libros[x].paginasLibro + "</h3></div></div></div><div align='center'><h2>Precio: $" + datosLibros.Libros[x].precioventa + "</h2></div>  <div>  <h2>Sinopsis: </h2> <br> <center> <p>" + datosLibros.Libros[x].sinopsisLibro + "</p> </center>  <br><center>" + datosLibros.Libros[x].generoLibro + "</center></div></div>");
+			 $('#contenido3').append("<div><div align='center'><div style='display:inline-block' align='center'><img src='http://192.168.1.191/proyecto/recursos/imagenes/libros/" + datosLibros.Libros[x].claveLibro + ".jpg' width='60%' height='auto' /></div><div style='display:inline-block' align='center'><h1>" + datosLibros.Libros[x].tituloLibro + "</h1><br><h2>" + datosLibros.Libros[x].autorLibro + "</h2><div style='display:inline-block' align='center'><h3>Editorial: " + datosLibros.Libros[x].editorialLibro + "</h3></div><div style='display:inline-block' align='center'><h3>Número de paginas: " + datosLibros.Libros[x].paginasLibro + "</h3></div></div></div><div align='center'><h2>Precio: $" + datosLibros.Libros[x].precioventa + "</h2></div>  <div>  <h2>Sinopsis: </h2> <br> <center> <p>" + datosLibros.Libros[x].sinopsisLibro + "</p> </center>  <br><center>" + datosLibros.Libros[x].generoLibro + "</center></div></div>");
 		 } 
 		 
 		} 
@@ -82,7 +82,7 @@ function consultaeditorial()
 	datos="editorial=" + $('#txteditorial').val();
     $.ajax({  
 		type: "POST",
-		url: "http://192.168.1.171/libreria/buscarEditorial.php",
+		url: "http://192.168.1.191/libreria/buscarEditorial.php",
 		data: datos, 
 		error: function() {  
 			alert("Error, no se puede conectar al servidor");
@@ -95,7 +95,7 @@ function consultaeditorial()
 			transition: 'pop' 
 		 });  
 		 for (var x=0; x<datosLibros.Libros.length; x++) { 6
-			 $('#contenido4').append("<div><div align='center'><div style='display:inline-block' align='center'><img src='http://192.168.1.171/proyecto/recursos/imagenes/libros/" + datosLibros.Libros[x].claveLibro + ".jpg' width='60%' height='auto' /></div><div style='display:inline-block' align='center'><h1>" + datosLibros.Libros[x].tituloLibro + "</h1><br><h2>" + datosLibros.Libros[x].autorLibro + "</h2><div style='display:inline-block' align='center'><h3>Editorial: " + datosLibros.Libros[x].editorialLibro + "</h3></div><div style='display:inline-block' align='center'><h3>Número de paginas: " + datosLibros.Libros[x].paginasLibro + "</h3></div></div></div><div align='center'><h2>Precio: $" + datosLibros.Libros[x].precioventa + "</h2></div>  <div>  <h2>Sinopsis: </h2> <br> <center> <p>" + datosLibros.Libros[x].sinopsisLibro + "</p> </center>  <br><center>" + datosLibros.Libros[x].generoLibro + "</center></div></div>");
+			 $('#contenido4').append("<div><div align='center'><div style='display:inline-block' align='center'><img src='http://192.168.1.191/proyecto/recursos/imagenes/libros/" + datosLibros.Libros[x].claveLibro + ".jpg' width='60%' height='auto' /></div><div style='display:inline-block' align='center'><h1>" + datosLibros.Libros[x].tituloLibro + "</h1><br><h2>" + datosLibros.Libros[x].autorLibro + "</h2><div style='display:inline-block' align='center'><h3>Editorial: " + datosLibros.Libros[x].editorialLibro + "</h3></div><div style='display:inline-block' align='center'><h3>Número de paginas: " + datosLibros.Libros[x].paginasLibro + "</h3></div></div></div><div align='center'><h2>Precio: $" + datosLibros.Libros[x].precioventa + "</h2></div>  <div>  <h2>Sinopsis: </h2> <br> <center> <p>" + datosLibros.Libros[x].sinopsisLibro + "</p> </center>  <br><center>" + datosLibros.Libros[x].generoLibro + "</center></div></div>");
 		 } 
 		 
 		} 
